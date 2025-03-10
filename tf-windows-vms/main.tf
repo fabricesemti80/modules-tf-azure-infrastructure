3b5efa96dcba3c0536132ac0672d4ebbf7cbdf5e
Fsemti on Aut-02 <fsemti@linkfinancial.eu>
2025-03-10 14:30:09 +0000

    feat: 🚀 add `vms` module

---
+++ tf-windows-vms/main.tf
@@ -0,0 +1,159 @@
+## Networking Setup
+
+# Network Interface for Windows Virtual Machines
+resource "azurerm_network_interface" "windows_nic" {
+  for_each            = var.windows_vms_config
+  name                = "${each.value.name}-nic"
+  location            = var.azure_region
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "${each.value.name}-ipconfig"
+    subnet_id                     = each.value.subnet_id
+    private_ip_address_allocation = each.value.static_ip != null ? "Static" : var.private_ip_allocation_method
+    private_ip_address            = each.value.static_ip
+  }
+
+  tags = merge(var.tags, {
+    CreationTimeUTC = timestamp()
+  })
+  lifecycle {
+    ignore_changes        = [tags["CreationTimeUTC"]]
+    create_before_destroy = false
+  }
+}
+
+
+## Virtual Machine Setup
+
+# Virtual Machine for Windows Deployment
+resource "azurerm_windows_virtual_machine" "windows_vm" {
+  for_each                          = var.windows_vms_config
+  name                              = each.value.name
+  computer_name                     = each.key
+  location                          = var.azure_region
+  resource_group_name               = var.resource_group_name
+  size                              = each.value.size
+  admin_username                    = each.value.login_username
+  admin_password                    = each.value.login_password
+  network_interface_ids             = [azurerm_network_interface.windows_nic[each.key].id]
+  patch_assessment_mode             = each.value.patch_assessment_mode
+  vm_agent_platform_updates_enabled = true
+  provision_vm_agent                = true
+
+  availability_set_id = each.value.availability_set_id
+  license_type        = var.windows_license_type
+
+  os_disk {
+    name                 = "${each.value.name}-osdisk"
+    caching              = each.value.os_disk_caching
+    storage_account_type = each.value.os_disk_storage_account_type
+  }
+
+  source_image_reference {
+    publisher = each.value.source_image_reference.publisher
+    offer     = each.value.source_image_reference.offer
+    sku       = each.value.source_image_reference.sku
+    version   = each.value.source_image_reference.version
+  }
+
+  # vtpm_enabled = true #TODO: Enable when we have a plan to support it
+
+  tags = var.tags
+  lifecycle {
+    ignore_changes = [
+      tags["CreationTimeUTC"],
+      identity,
+      vm_agent_platform_updates_enabled
+    ]
+  }
+
+  depends_on = [
+    azurerm_network_interface.windows_nic
+  ]
+}
+
+# Data Disks for Windows Virtual Machines
+resource "azurerm_managed_disk" "windows_data_disk" {
+  for_each = {
+    for idx in flatten([
+      for vm_key, vm in var.windows_vms_config : [
+        for disk in vm.data_disks : {
+          vm_key     = vm_key
+          disk_name  = disk.name
+          disk_size  = disk.size_gb
+          disk_type  = disk.storage_account_type
+          disk_cache = disk.caching
+          disk_lun   = disk.lun
+        }
+      ]
+    ]) : "${idx.vm_key}-${idx.disk_name}" => idx
+  }
+
+  name                 = each.value.disk_name
+  location             = var.azure_region
+  resource_group_name  = var.resource_group_name
+  storage_account_type = each.value.disk_type
+  create_option        = "Empty"
+  disk_size_gb         = each.value.disk_size
+
+  tags = var.tags
+  lifecycle {
+    ignore_changes        = [tags["CreationTimeUTC"]]
+    create_before_destroy = false
+  }
+
+}
+
+# Attach Data Disks to Windows Virtual Machines
+resource "azurerm_virtual_machine_data_disk_attachment" "windows_disk_attachment" {
+  for_each = {
+    for idx in flatten([
+      for vm_key, vm in var.windows_vms_config : [
+        for disk in vm.data_disks : {
+          vm_key     = vm_key
+          disk_name  = disk.name
+          disk_cache = disk.caching
+          disk_lun   = disk.lun
+        }
+      ]
+    ]) : "${idx.vm_key}-${idx.disk_name}" => idx
+  }
+
+  managed_disk_id    = azurerm_managed_disk.windows_data_disk[each.key].id
+  virtual_machine_id = azurerm_windows_virtual_machine.windows_vm[each.value.vm_key].id
+  lun                = each.value.disk_lun
+  caching            = each.value.disk_cache
+
+  depends_on = [
+    azurerm_managed_disk.windows_data_disk
+  ]
+}
+
+## VM Auto-shutdown Schedules
+
+# Schedule for Auto-shutdown of Windows Virtual Machines
+resource "azurerm_dev_test_global_vm_shutdown_schedule" "windows_shutdown" {
+  for_each = {
+    for k, v in var.windows_vms_config : k => v
+    if v.daily_shutdown_time != null
+  }
+  virtual_machine_id    = azurerm_windows_virtual_machine.windows_vm[each.key].id
+  location              = var.azure_region
+  enabled               = true
+  daily_recurrence_time = each.value.daily_shutdown_time
+  timezone              = "UTC"
+
+  notification_settings {
+    enabled = false
+  }
+
+  tags = var.tags
+  lifecycle {
+    ignore_changes = [tags["CreationTimeUTC"]]
+  }
+
+  depends_on = [
+    azurerm_windows_virtual_machine.windows_vm
+  ]
+}
---
+++ tf-windows-vms/variables.tf
@@ -0,0 +1,160 @@
+##? Configuration for Windows Virtual Machines
+
+# Define the configuration for Windows Virtual Machines including VM name, size, and subnet ID.
+variable "windows_vms_config" {
+  description = <<EOT
+  A map of Windows Virtual Machines configurations where each key is a unique identifier and the value contains the VM's specifications.
+  Each VM configuration includes details such as the VM name, size, subnet ID, source image reference, optional data disks configuration, and optional daily shutdown time in UTC 24hr format (e.g., '2000' for 8:00 PM).
+
+  Example usage:
+  windows_vms_config = {
+    "vm1" = {
+      name           = "example-vm1"
+      login_username = "azureuser"
+      login_password = "Password123!"
+      source_image_reference = {
+        publisher = "MicrosoftWindowsServer"
+        offer     = "WindowsServer"
+        sku       = "2022-Datacenter"
+        version   = "latest"
+      }
+      data_disks = []
+    }
+  }
+  EOT
+
+  type = map(object({
+    name           = string                           # The name of the virtual machine
+    login_username = optional(string, "azureuser")    # The username for logging into the VM
+    login_password = string                           # The password for logging into the VM
+    size           = optional(string, "Standard_B2s") # The size of the virtual machine
+    subnet_id      = optional(string, "")             # The subnet ID where the VM will be deployed
+    source_image_reference = object({
+      publisher = string # The publisher of the source image
+      offer     = string # The offer of the source image
+      sku       = string # The SKU of the source image
+      version   = string # The version of the source image
+    })
+    static_ip                    = optional(string)                        # For static IP allocation (not used if private_ip_allocation_method is set to Dynamic)
+    patch_assessment_mode        = optional(string, "AutomaticByPlatform") # The patch assessment mode for the VM
+    availability_set_id          = optional(string, null)                  # The availability set ID for the VM
+    os_disk_storage_account_type = optional(string, "Standard_LRS")        # The storage account type for the OS disk
+    os_disk_caching              = optional(string, "ReadWrite")           # The caching mode for the OS disk
+    daily_shutdown_time          = optional(string)                        # The daily shutdown time in UTC 24hr format (e.g., '2000' for 8:00 PM). Set to null to disable auto-shutdown
+    data_disks = optional(list(object({
+      name                 = string                           # The name of the data disk
+      size_gb              = number                           # The size of the data disk in GB
+      storage_account_type = optional(string, "Standard_LRS") # The storage account type for the data disk
+      caching              = optional(string, "ReadWrite")    # The caching mode for the data disk
+      lun                  = number                           # The logical unit number for the data disk
+    })), [])
+  }))
+}
+
+##? General Azure Configuration
+
+# Resource Group Name for placing all compute resources like VMs
+variable "resource_group_name" {
+  description = <<EOT
+  The name of the resource group where compute resources (such as virtual machines) will be placed.
+  Resource group names:
+  - Must be unique within your Azure subscription
+  - Can be 1-90 characters long
+  - Can only include alphanumeric, underscore, parentheses, hyphen, period
+  - Cannot end in a period
+  EOT
+  type        = string
+}
+
+# Azure Region where resources will be deployed (e.g., 'East US', 'West Europe')
+variable "azure_region" {
+  description = <<EOT
+  The Azure region where the resources will be deployed.
+  Use the official Azure region names such as:
+  - "eastus"
+  - "westeurope"
+  - "southcentralus"
+  EOT
+  type        = string
+}
+
+# # Prefix used for creating unique names for Azure resources (e.g., 'bcmg-az-vm1')
+# variable "resource_name_prefix" {
+#   description = <<EOT
+#   A prefix string that will be added to the names of Azure resources to help create unique names.
+#   Example: 'bcmg-az-vm1'
+#   EOT
+#   type        = string
+#   default     = "bcmg-az"
+# }
+
+# Tags that will be applied to the resources for better management
+variable "tags" {
+  description = <<EOT
+  A map of tags to be applied to resources for easier management and organization.
+  Tags are key-value pairs that help with:
+  - Resource organization
+  - Cost allocation
+  - Automation
+  - Access control
+
+  Example:
+  resource_tags = {
+    Environment = "Production"
+    Department  = "IT"
+    Project     = "Infrastructure"
+    Owner       = "DevOps Team"
+  }
+  EOT
+  type        = map(string)
+}
+
+##? Administrative Configuration
+
+# Private IP address allocation method for the network interfaces (can be 'Static' or 'Dynamic')
+variable "private_ip_allocation_method" {
+  description = <<EOT
+  The method used to allocate private IP addresses for network interfaces.
+  Can be 'Static' (manual IP) or 'Dynamic' (automatic IP).
+  EOT
+  type        = string
+  default     = "Dynamic"
+}
+
+##? Security & Secret Management
+
+# ID for the Azure Key Vault used to store secrets such as RDP passwords
+variable "key_vault_id" {
+  description = <<EOT
+  The ID for the Azure Key Vault used to store secrets such as RDP passwords.
+  EOT
+  type        = string
+  default     = ""
+}
+
+# The time of day when the virtual machines should be shut down daily in UTC 24hr format (e.g., '2000' for 8:00 PM). Set to null to disable auto-shutdown.
+variable "vm_daily_shutdown_time" {
+  description = <<EOT
+  The time of day when the virtual machines should be shut down daily in UTC 24hr format (e.g., '2000' for 8:00 PM).
+  Set to null to disable auto-shutdown.
+  EOT
+  type        = string
+  default     = null
+}
+
+# Specifies the type of on-premise license (BYOL) to be used
+# Ensure you have eligible Windows Server licenses with Software Assurance before enabling
+# The license_type can be either "Windows_Server" or "None"
+# This change won't require a VM restart
+# Remember to validate your organization's licensing compliance before enabling
+variable "windows_license_type" {
+  description = <<EOT
+  Specifies the type of on-premise license (BYOL) to be used.
+  Ensure you have eligible Windows Server licenses with Software Assurance before enabling.
+  The license_type can be either "Windows_Server" or "None".
+  This change won't require a VM restart.
+  Remember to validate your organization's licensing compliance before enabling.
+  EOT
+  type        = string
+  default     = "None"
+}
